5a501907fc74a04f3b3eefb65d5c42b55df354a0
huyenle15592 <huyenle15592@gmail.com>
2026-06-26 15:09:57 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,6 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  vite: {
-    base: '/tichdiem.trison/',
-  },
-  // Đổi TanStack sang chế độ đóng gói static phục vụ cho GitHub Pages
   tanstackStart: {
     server: { 
       preset: "static" 
